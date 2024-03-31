cb63087fbdaaddf27d08c1c8e98271a9a9ac5efd
Y0z64 <yairprogrammer@gmail.com>
2024-03-31 11:43:19 -0600

    Deleted unused vars

--- src/main.ts
+++ src/main.ts
@@ -10,26 +10,9 @@ const name = document.createElement("h1");
 
 const icon = document.querySelector("#icon");
 
-var cursor = document.querySelector(".cursor"),
-  mouseX = 0,
-  mouseY = 0;
-
 // Content
 name.innerText = "Y0z64";
 name.classList.add("name");
-
-gsap.to({}, 0.016, {
-  repeat: -1,
-
-  onReapeat: function () {
-    gsap.set(cursor, {
-      css: {
-        left: mouseX,
-        top: mouseY,
-      },
-    });
-  },
-});
 
 // Append
 appDiv?.appendChild(content);
@@ -66,11 +49,6 @@ icon?.addEventListener("mouseleave", () => {
     scale: 1,
     ease: "power2.out",
   });
-});
-
-window.addEventListener("mousemove", function (e) {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
 });
 
 setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
