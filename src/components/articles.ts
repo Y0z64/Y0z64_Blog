95be71d8c2661f6a6560f2f116c3e9639b66fb22
Y0z64 <yairprogrammer@gmail.com>
2024-04-01 18:07:45 -0600

    Card fix

--- src/components/articles.ts
+++ src/components/articles.ts
@@ -1,54 +1,5 @@
-type Card = {
-  title: string;
-  description: string;
-  tag?: string;
-  image?: string;
-};
+import { Card, createCard } from "./card";
 
-export function createCard(article: Card) {
-  console.log("Creating a card");
-  // Create elements
-  const text = document.createElement("span");
-  const card = document.createElement("div");
-  const title = document.createElement("h3");
-  const description = document.createElement("p");
-  let tag = document.createElement("span");
-  let image = document.createElement("img");
-
-  if (article.tag) {
-    tag = document.createElement("span");
-    tag.classList.add("tag");
-    tag.textContent = article.tag;
-    card.appendChild(tag);
-    card.style.paddingBottom = "26.5px"
-  }
-
-  // Add an image if it exists
-  if (article.image) {
-    image = document.createElement("img");
-    image.src = article.image;
-    card.appendChild(image);
-  }
-
-
-  // Add classes
-  card.classList.add("card");
-  text.classList.add("hero-text");
-  title.classList.add("title");
-  description.classList.add("description");
-
-  // Content
-  title.textContent = article.title;
-  description.textContent = article.description;
-
-  // Connections
-  text.appendChild(title);
-  text.appendChild(description);
-
-  card.appendChild(text);
-
-  return card;
-}
 
 export default function createArticles(articles: Card[]) {
   const content = document.createElement("div");
---
+++ src/components/card.ts
@@ -0,0 +1,50 @@
+export type Card = {
+  title: string;
+  description: string;
+  tag?: string;
+  image?: string;
+};
+
+export function createCard(article: Card) {
+  console.log("Creating a card");
+  // Create elements
+  const text = document.createElement("span");
+  const card = document.createElement("div");
+  const title = document.createElement("h3");
+  const description = document.createElement("p");
+  let tag = document.createElement("span");
+  let image = document.createElement("img");
+
+  if (article.tag) {
+    tag = document.createElement("span");
+    tag.classList.add("tag");
+    tag.textContent = article.tag;
+    card.appendChild(tag);
+    card.style.paddingBottom = "26.5px";
+  }
+
+  // Add an image if it exists
+  if (article.image) {
+    image = document.createElement("img");
+    image.src = article.image;
+    card.appendChild(image);
+  }
+
+  // Add classes
+  card.classList.add("card");
+  text.classList.add("hero-text");
+  title.classList.add("title");
+  description.classList.add("description");
+
+  // Content
+  title.textContent = article.title;
+  description.textContent = article.description;
+
+  // Connections
+  text.appendChild(title);
+  text.appendChild(description);
+
+  card.appendChild(text);
+
+  return card;
+}
